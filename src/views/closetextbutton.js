29f912d250b9b20a2419325be396cfdb4077a75d
yapcheahshen <yapcheahshen@gmail.com>
2015-10-19 01:21:54 +0800

    closebutton css class, fix RangeHyperLink no key warning

--- src/views/closetextbutton.js
+++ src/views/closetextbutton.js
@@ -1,11 +1,7 @@
 var React=require("react");
-var styles={
-	closebutton :
-	{borderRadius:"3px",background:"#AF1010",color:"white",cursor:"pointer"}
-}
 
 module.exports = class CloseTextButton extends React.Component {
 	render () {
-		return <span style={styles.closebutton} onClick={this.props.onClose}>×</span>
+		return <span title="Ctrl+Q" className="closebutton" onClick={this.props.onClose}>×</span>
 	}
 }
